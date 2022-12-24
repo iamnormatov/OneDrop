4dc432956be92c8573c07077492ab1be17a17340
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-24 18:43:11 +0500

    authstore

--- src/components/store/AuthStore.js
+++ src/components/store/AuthStore.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { auth } from '../../firebase';
-
+// .
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
