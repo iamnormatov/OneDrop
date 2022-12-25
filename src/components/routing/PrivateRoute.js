d95d2ec36e3abc5cc8d3695bf09d8df6c95eca3c
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-25 10:49:33 +0500

    privaterouter

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from './../store/AuthStore';
 import { Navigate } from 'react-router-dom';
-// .
+// ..
 const PrivateRoute = ({ children }) => {
     const { currentUser } = useAuth();
 
