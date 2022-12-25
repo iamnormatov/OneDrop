31bcf635ceb2bab7055c3fd4da0f713887237601
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-25 10:54:12 +0500

    alert

--- src/ui/alerts/Alert.js
+++ src/ui/alerts/Alert.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AlertError from './AlertError';
 import AlertSuccess from './AlertSuccess';
-// .
+// ..
 const Alert = ({ type, children }) => {
     const typeOfAlert = {
         error: <AlertError content={children}/>,
