10fa461b842c8f85d284152a1dcd59598fc93a5a
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-25 10:52:54 +0500

    forgot

--- src/pages/Auth/ForgotPassword.js
+++ src/pages/Auth/ForgotPassword.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useAuth } from '../../components/store/AuthStore';
 import { Oval } from 'react-loader-spinner';
 import Alert from '../../ui/alerts/Alert';
-// .
+// ..
 const ForgotPassword = () => {
     const emailRef = useRef();
     const { resetPassword } = useAuth();
