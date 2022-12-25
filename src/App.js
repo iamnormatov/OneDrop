e650ec3c0a510bf86fe5ca49491050bd8bb7563d
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-25 10:54:59 +0500

    app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppRouter from "./components/routing/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import AuthStore from "./components/store/AuthStore";
-// .
+// ..
 function App() {
   return (
     <AuthStore>
